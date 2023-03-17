6c7848be03e4e3cd50216058c521e807e54370e6
Fatemeboroumand <121131737+Fatemeboroumand@users.noreply.github.com>
2023-03-17 22:09:37 +0330

    crest react app

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Weather />
+      <footer>
+      This project was coded by Fateme Boroumand and is <a href=''>open-sourced on GitHub</a> and hosted on Netlify
+      </footer>
     </div>
   );
 }
