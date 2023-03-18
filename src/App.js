adb4dfee0e5617e908851f3b0fcc6d25a7d3d380
Fatemeboroumand <121131737+Fatemeboroumand@users.noreply.github.com>
2023-03-18 16:45:59 +0330

    Add rel="noreferrer"

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <Weather defaultCity="paris"/>
       <footer>
-      This project was coded by Fateme Boroumand and is <a href='https://github.com/Fatemeboroumand/my-weather-react-app' target="_blank">open-sourced on GitHub</a> and hosted on <a href=''>Netlify</a>
+      This project was coded by Fateme Boroumand and is <a href='https://github.com/Fatemeboroumand/my-weather-react-app' target="_blank" rel="noreferrer">open-sourced on GitHub</a> and hosted on <a href=''>Netlify</a>
       </footer>
     </div>
   );
