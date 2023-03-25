dce620e00b7b33700a64fce29d3a3ac15de6464e
Fatemeboroumand <121131737+Fatemeboroumand@users.noreply.github.com>
2023-03-25 16:21:15 +0430

    Add netlify link

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <Weather defaultCity="paris"/>
       <footer>
-      This project was coded by Fateme Boroumand and is <a href='https://github.com/Fatemeboroumand/my-weather-react-app' target="_blank" rel="noreferrer">open-sourced on GitHub</a> and hosted on Netlify
+      This project was coded by Fateme Boroumand and is <a href='https://github.com/Fatemeboroumand/my-weather-react-app' target="_blank" rel="noreferrer">open-sourced on GitHub</a> and hosted on <a href='https://master--melodious-moonbeam-d20331.netlify.app/' target="_blank" rel='noreferrer'>Netlify</a>
       </footer>
     </div>
   );
